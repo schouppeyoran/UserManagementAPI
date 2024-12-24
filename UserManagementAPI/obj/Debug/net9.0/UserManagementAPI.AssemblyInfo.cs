c1dd4a671fd226e542b8e35ccbebe9d04a8d7c76
Yoran Desktop <schouppey@gmail.com>
2024-12-24 02:00:50 +0100

    add: test calls file

--- UserManagementAPI/obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
+++ UserManagementAPI/obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e3e7aaa65669d42fb89a61852407a091d42294")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
